b284079173e0025b6bd10e831cbe1b7b47453b7a
xjchenhao <wy.chenhao@qq.com>
2020-07-28 23:07:23 +0800

    完成产品详情页面

--- src/app/pages/product-detail/product-detail.component.ts
+++ src/app/pages/product-detail/product-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from "@angular/core";
+import { ToastService } from "ng-zorro-antd-mobile";
+import request from "src/app/utils/request.js";
+const URL = require("url");
 
 @Component({
   selector: "app-product-detail",
@@ -6,22 +9,52 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./product-detail.component.less"],
 })
 export class ProductDetailComponent implements OnInit {
-  data = [
-    "AiyWuByWklrrUDlFignR",
-    "TekJlZRVCjLFexlOCuWn",
-    "IJOtIlfsYdTyaDTRVrLI",
-  ];
+  public data: object = {};
 
-  imgHeight = "184px";
+  public id: String = "";
+  public imgHeight: String = "184px";
+  constructor(private _toast: ToastService) {}
+  async ngOnInit() {
+    const urlObj = URL.parse(window.location.href, true);
+    const { id } = urlObj.query;
 
-  product = {
-    content: `<ul><li>555</li></ul>`,
-  };
-  constructor() {}
+    this.id = id;
 
-  ngOnInit() {}
+    const result = await request({
+      url: `/api/product/detail?id=${id}`,
+      type: "get",
+    });
+
+    if (result.code !== "0") {
+      const toast = this._toast.show(result.msg, 1000);
+
+      return;
+    }
+
+    this.data = result.data;
+  }
 
   onBack() {
     history.back();
+  }
+
+  async onSubmit() {
+    const result = await request({
+      url: "/api/order/create",
+      type: "post",
+      data: {
+        productId: this.id,
+      },
+    });
+
+    if (result.code !== "0") {
+      const toast = this._toast.show(result.msg, 1000);
+
+      return;
+    }
+
+    this._toast.success("支付成功，即将返回首页", 3000, () => {
+      location.href = "/";
+    });
   }
 }
