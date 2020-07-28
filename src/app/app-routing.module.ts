e66d4d6412eb65d2303ee6d8876c3c17342cee45
xjchenhao <wy.chenhao@qq.com>
2020-07-28 15:32:48 +0800

    归零

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 
-import { MyComponent } from "./components/my/my.component";
-
-const routes: Routes = [{ path: "my", component: MyComponent }];
+const routes: Routes = [];
 
 @NgModule({
-  declarations: [MyComponent],
+  declarations: [],
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
 })
